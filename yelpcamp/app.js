81e790c317a39ae56e328301441500bbc95c3b99
John Ramirez <cyclopsvision87@icloud.com>
2017-03-14 22:57:45 -0600

    Added in support for comments on a per campground basis.
    added in a style sheet. for now, we're just going to be using a Google
    font for everything.
    
    renamed some files to match <object>.<routetype>.ejs naming convention.

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -6,18 +6,28 @@ var app        = require('./config/express')(),
 // MONGOOSE STUFF
 var mongoose   = require('./config/mongoose')();
 var Campground = require('./models/campground.model');
+var Comment    = require('./models/comment.model');
 
 // Initial DB with seed database
 var seedDB = require("./seed_db");
 seedDB();
 
+
+// Summary of RESTful routes
+//
+// name                         HTTP Verb
+// ==========================================================================
+// INDEX   /campgrounds           GET
+// NEW     /campgrounds/new       GET
+// CREATE  /campgrounds           POST
+// SHOW    /campgrounds/:id       GET
+// EDIT    /campgrounds/:id/edit  GET
+// UPDATE  /campgrounds/:id       PUT
+// DESTROY /campgrounds/:id       DELETE
+//
+//
 app.get('/', function(req, res) {
     res.render('landing');
-});
-
-// NEW route
-app.get('/campgrounds/new', function(req, res) {
-    res.render("new");    
 });
 
 // INDEX route
@@ -27,20 +37,14 @@ app.get('/campgrounds', function(req, res) {
             console.log("Couldn't fetch campgrounds from DB!!")
             console.log(err);
         } else {
-            res.render('campgrounds', { campgrounds: campgrounds});
+            res.render('campground.index.ejs', { campgrounds: campgrounds});
         }
     });
 });
 
-// SHOW route
-app.get('/campgrounds/:id', function(req, res) {
-    Campground.findById(req.params.id).populate('comments').exec(function(err, campground) {
-        if (err) {
-            console.log("app.get-campground/id, err: " + err);
-        } else {
-            res.render('campground.details.ejs', {campground: campground});
-        }
-    });
+// NEW route
+app.get('/campgrounds/new', function(req, res) {
+    res.render("campground.new.ejs");    
 });
 
 // CREATE route
@@ -67,7 +71,54 @@ app.post('/campgrounds', function(req,res) {
     }
     res.redirect('/campgrounds');
 });
- 
+
+// SHOW route
+app.get('/campgrounds/:id', function(req, res) {
+    Campground.findById(req.params.id).populate('comments').exec(function(err, campground) {
+        if (err) {
+            console.log("app.get-campground/id, err: " + err);
+        } else {
+            res.render('campground.show.ejs', {campground: campground});
+        }
+    });
+});
+
+// ===============================================
+// ******       COMMENTS ROUTES          *********
+// ===============================================
+// RESTful routes for campground comments
+// NEW     /campgrounds/:id/comments/new  GET
+// CREATE  /campgrounds/:id/comments      POST
+
+app.get('/campgrounds/:id/comments/new', function(req, res) {
+    Campground.findById(req.params.id, function(err, campground){
+        if (err) {
+            console.error("/campground/:id/comments/new - could not findById: "+err);
+        } else {
+            res.render('comment.new.ejs', {campground: campground});
+        }
+    });
+});
+
+app.post('/campgrounds/:id/comments', function(req, res) {
+    Campground.findById(req.params.id, function(err, campground){
+        if (err) {
+            console.error("/campground/:id/comments - could not findById: "+err);
+            res.redirect('/');
+        } else {
+            Comment.create(req.body.comment, function(err2, _comment) {
+                if (err2) {
+                    console.error("Couldn't create Comment.");
+                } else {
+                    campground.comments.push(_comment._id);
+                    campground.save();
+                }
+            });
+            res.redirect('/campgrounds/' + campground._id);
+        }
+    });
+});
+
 app.listen(config.port, config.ip, function() {
    console.log("YelpCamp server starting up... " + config.ip); 
 });
