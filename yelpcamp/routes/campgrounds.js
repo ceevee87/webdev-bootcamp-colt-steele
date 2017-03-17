9339ab3c7ab90740dfa1ccde4ba0560f1e5af06f
John Ramirez <cyclopsvision87@icloud.com>
2017-03-17 10:42:45 -0600

    first version of implementing authorization. this change is only for editing details of a single campground. to do this, I check if the user logged in and if so whether they are the owner of the campground. only then do I allow them to edit the campground.

--- yelpcamp/routes/campgrounds.js
+++ yelpcamp/routes/campgrounds.js
@@ -80,14 +80,36 @@ router.get('/:id', function(req, res) {
 
 // EDIT route
 router.get('/:id/edit', function(req, res) {
-    Campground.findById(req.params.id).populate('comments').exec(function(err, campground) {
-        if (err) {
-            req.flash("error", "could not find campground by id: "+ err.message);
-            res.redirect('/campgrounds')
-        } else {
-            res.render('campground.edit.ejs', {campground: campground});
-        }
-    });
+    // let's just find out if the user is even logged in before
+    // we start doing any updates
+    if (!req.isAuthenticated()) {
+        req.flash("error", "You need to be logged in before editing a campground.");
+        res.redirect('/campgrounds/' + req.params.id);
+    } else {
+        Campground.findById(req.params.id).populate('comments').exec(function(err, campground) {
+            if (err) {
+                req.flash("error", "could not find campground by id: "+ err.message);
+                res.redirect('/campgrounds')
+            } else if (!campground.author.id || (campground.author.id && 
+                        campground.author.id.equals(req.user._id))) {
+                // we should only be allowing owners of the campground to
+                // make edits. what about a strange state where no owner
+                // is defined? This is definitely possible in early dev rounds
+                // during testing. 
+                // in this case, let the first editor "claim" the campground.
+                if (!campground.author.id) {
+                    campground.author.id = req.user._id;
+                    campground.author.username = req.user.username;
+                    campground.save();
+                }
+                res.render('campground.edit.ejs', {campground: campground});
+            } else {
+                req.flash("error", 
+                "You are not authorized to edit this campground.<" + req.user.username +">");
+                res.redirect('/campgrounds/' + req.params.id);
+            }
+        });
+    }
 });
 
 // UPDATE route
