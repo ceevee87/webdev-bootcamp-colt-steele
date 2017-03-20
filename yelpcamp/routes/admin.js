41adefa60e03a81a9968e70f08fd489cb5f945be
John Ramirez <cyclopsvision87@icloud.com>
2017-03-20 13:36:24 -0600

    write out campground and comments dbs with createdAt times.

--- yelpcamp/routes/admin.js
+++ yelpcamp/routes/admin.js
@@ -16,6 +16,7 @@ var farmSchema = mongoose.Schema({
     price:       String,
     image:       String,
     description: String,
+    createdAt: { type: Date, default: Date.now },
     author: { id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
             username: String },
     comments:    [
@@ -27,6 +28,12 @@ var farmSchema = mongoose.Schema({
 var Farm = mongoose.model('farm', farmSchema);
 
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 router.get('/campgrounds/admin/dump', function(req, res) {
     Campground.find({}, { _id: 0, __v: 0}, function(err, campgrounds) {
         if (err) {
@@ -34,11 +41,13 @@ router.get('/campgrounds/admin/dump', function(req, res) {
             console.log(err);
         } else {
             campgrounds.forEach(function(campground) {
+                var xx = new Date().getTime() - 1000*getRandomInt(10000, 12*86400);
                 var cg2 = {
                     "name": campground.name,
                     "price": campground.price,
                     "image": campground.image,
                     "description": campground.description,
+                    "createdAt" : new Date(xx),
                     "comments": [],
                     "author": {
                         "id": new mongoose.mongo.ObjectID(campground.author.id),
@@ -49,8 +58,6 @@ router.get('/campgrounds/admin/dump', function(req, res) {
                 campground.comments.forEach(function(cm){
                     cg2.comments.push(new mongoose.mongo.ObjectID(cm));
                 });
-
-                console.log(JSON.stringify(cg2,null,'\t'));
                 Farm.create(cg2, function(err, farm) {
                     if (err) {
                         console.log("Shit's gone totally haywire!!\n"+err.message);
@@ -61,6 +68,46 @@ router.get('/campgrounds/admin/dump', function(req, res) {
                 });
             });
             res.render('campground.index.ejs', { campgrounds: campgrounds});
+        }
+    });
+});
+
+
+var remarkSchema = mongoose.Schema(
+    {
+        text  : String,
+        createdAt : { type: Date, default: Date.now },
+        author: { id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
+                username: String }
+    }
+);
+var Remark = mongoose.model('Remark', remarkSchema);
+
+router.get('/comments/admin/dump', function(req, res) {
+    Comment.find({}, { _id: 0, __v: 0}, function(err, comments) {
+        if (err) {
+            console.log("Couldn't fetch comments from DB!!")
+            console.log(err);
+        } else {
+            comments.forEach(function(comment) {
+                var xx = new Date().getTime() - 1000*getRandomInt(50000, 16*86400); 
+                var cm2 = {
+                    "text": comment.text,
+                    "createdAt" : new Date(xx),
+                    "author": new mongoose.mongo.ObjectID(comment.author.id),
+                };
+                // console.log(JSON.stringify(cm2, null, '\t'));
+
+                Remark.create(cm2, function(err, remark) {
+                    if (err) {
+                        console.log("Shit's gone totally haywire!!\n"+err.message);
+                    } else {
+                        console.log("Created new remark!");
+                        console.log(JSON.stringify(remark, null,'\t'));
+                    }
+                });
+            });
+            res.redirect('/campgrounds');
         }
     });
 });
