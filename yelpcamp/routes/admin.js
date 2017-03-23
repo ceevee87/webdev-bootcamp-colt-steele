a45aeb282b177ef01588244404f11cfabb5ee9f0
John Ramirez <cyclopsvision87@icloud.com>
2017-03-23 14:19:40 -0600

    rough cut version of restoring a db from a JSON file!

--- yelpcamp/routes/admin.js
+++ yelpcamp/routes/admin.js
@@ -100,6 +100,84 @@ router.get('/comments/admin/cruser', function(req, res) {
     });
 });
 
+
+router.get('/comments/admin/crdb', function(req, res) {
+    var fname   = './campgrounds.json';
+    User.find({}, {username : 1, _id : 1}, function(err, users) {
+        if (err) {
+            console.error(">>>>>>>>>>>>>>> USER LOOKUP ERROR <<<<<<<<<<<<<<< ");
+        } else {
+            var authors = {};
+            users.forEach(function(u){
+                authors[u.username] = {
+                    id : mongoose.mongo.ObjectID(u._id),
+                    username: u.username
+                }
+            });
+            fs.readFile(fname, 'utf-8', function(err, data) {
+                if (err) {
+                    console.error(">>>>>>>>>>>>>>> YIKES!!!! ERROR!!! <<<<<<<<<<<<<<< ");
+                } else {
+                    // console.log("Data from file:\n", data);
+                    var obj = JSON.parse(data);
+                    console.log("Number of campgrounds = "+obj.length);
+                    Object.getOwnPropertyNames(obj).sort().forEach(
+                        function (val, idx, array) {
+                            if (isNaN(val)) { return; } 
+                            var campground = obj[val];
+
+                            if (!campground.author || !campground.author.username 
+                                || !authors[campground.author.username]) { return ; }
+
+                            // console.log("campground author = " + 
+                            //     JSON.stringify(authors[campground.author.username]));
+                            
+                            var newCg = {
+                                name : campground.name,
+                                price : campground.price,
+                                image: campground.image,
+                                description : campground.description,
+                                comments : [],
+                                author : authors[campground.author.username],
+                                createdAt : campground.createdAt
+                            };
+
+                            campground.comments.forEach(function(comment){
+                                if (!comment.author || !comment.author.username) { return ; }
+                                if (authors[comment.author.username]) {
+                                    var newComm = {
+                                        _id : mongoose.mongo.ObjectID(),
+                                        text : comment.text,
+                                        createdAt : comment.createdAt,
+                                        author: authors[comment.author.username]
+                                    };
+                                    var commList = [];
+                                    Comment.create(newComm, function(err, _comment) {
+                                        if (err) {
+                                            // do nothing
+                                        }
+                                    });
+
+                                    newCg.comments.push(newComm._id);
+                                }
+                            });
+                            // console.log("NEW CAMPGROUND TO CREATE: " + JSON.stringify(newCg,null,'\t') + '\n');
+                            Campground.create(newCg, function(err, newcampground) {
+                                if (err) {
+                                    console.error(">>>>>####### WHOOOOPS !!! ########## <<<<<<<<<<<");
+                                } else {
+                                    console.log("created new cg: "+newcampground+'\n');
+                                }
+                            });
+                        }
+                    );
+                    res.redirect('/campgrounds');            
+                }
+            });
+        }
+    });
+});
+
 router.get('/campgrounds/admin/dump2db', function(req, res) {
     Campground.find({}, { _id: 0, __v: 0}, function(err, campgrounds) {
         if (err) {
