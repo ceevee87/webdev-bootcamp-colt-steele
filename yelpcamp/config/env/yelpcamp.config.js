176f9812aaea66088d02db57f508835254307bfd
John Ramirez <cyclopsvision87@icloud.com>
2017-04-20 13:55:34 -0600

    make mongodb url name configurable via unix env var.

--- yelpcamp/config/env/yelpcamp.config.js
+++ yelpcamp/config/env/yelpcamp.config.js
@@ -1,9 +1,10 @@
 
 var port = process.env.PORT || 3000;
 var ip   = process.env.IP || 'localhost';
+var db   = process.env.MONGODB_URI || 'mongodb://localhost/yelpcamp';
 
 module.exports = {
-    db: 'mongodb://localhost/yelpcamp',
+    db: db,
     port: port,
     ip: ip,
     usersJsonFile: 'users.json',
